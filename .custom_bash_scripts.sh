0a1f66a98eba990692671e3aab9b8b05b70aeb3a
Sheldon Lee <sheldon.m.lee@gmail.com>
2020-06-29 01:38:46 +0100

    f_scrot/s_scrot now addes seconds to file name.

--- .custom_bash_scripts.sh
+++ .custom_bash_scripts.sh
@@ -2,15 +2,21 @@
 
 #screenshots using scrot
 scrot_dir=$HOME/screenshots/
-
+date_format='%Y-%m-%d_%H-%M-%S'
 s_scrot() {
-	name=$(date +%Y-%m-%d_%H-%M_sel.png)
+	name=$(date +${date_format}_sel.png)
 	path="${scrot_dir}${name}"
 	scrot -s $path
 }
 
 f_scrot() {
-	name=$(date +%Y-%m-%d_%H-%M_full.png)
+	name=$(date +${date_format}_full.png)
 	path="${scrot_dir}${name}"
 	scrot $path
+}
+
+#cd into config dir
+cfdir()
+{
+	cd $HOME/.config/$1
 }
